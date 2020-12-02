a292dd05d49b8d55c4b1fe72f778160bc76814ae
paikwiki <paikwiki@gmail.com>
2020-12-02 15:06:07 +0900

    Create a simple website: the list of web-portfolio

--- codes/src/App.js
+++ codes/src/App.js
@@ -1,38 +1,11 @@
 import './scss/App.scss';
+import { appData } from './appData';
 import Card from './Card';
 import Header from './Header';
 import tw from 'twin.macro';
 import { css } from 'styled-components/macro'
 
-const appData = {
-  pageTitle: 'The painters',
-  cards: [
-    {
-      title: 'Salvador Dalí',
-      description: 'Hello, I am a description.',
-      imageUrl: 'https://picsum.photos/640/360',
-      buttonText: 'website',
-    },
-    {
-      title: 'Vincent van Gogh',
-      description: 'Hello, I am a description.',
-      imageUrl: 'https://picsum.photos/960/540',
-      buttonText: 'website',
-    },
-    {
-      title: 'Pablo Picasso',
-      description: 'Hello, I am a description.',
-      imageUrl: 'https://picsum.photos/480/270',
-      buttonText: 'website',
-    },
-    {
-      title: 'Gustav Klimt',
-      description: 'Hello, I am a description.',
-      imageUrl: 'https://picsum.photos/496/279',
-      buttonText: 'website',
-    },
-  ],
-};
+
 
 function App() {
   return (
